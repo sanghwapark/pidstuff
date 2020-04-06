9abf945e68e7778b6e4eec07bee8a236bd0a7b79
Sanghwa Park <sanghwapark@gmail.com>
2020-04-06 13:01:18 -0400

    Update example macro

--- dirclike/run_sim.C
+++ dirclike/run_sim.C
@@ -1,16 +1,32 @@
-#include "Sim.C"
+R__LOAD_LIBRARY(libPIDToy)
 
-void run_sim(int pid)
+void run_sim(int pid, double p, int nevt=1)
 {
 
-  Sim ana(pid);
+  ToyCherenkov* ana = new ToyCherenkov(pid);
+  ana->SetVerbosity(0);
+  ana->SetDetector(1.02, 20., 2., -10);
+  ana->SetCutoff(300.);
+  ana->SetdT(10.);
 
-  PIDEvent* anEvent = new PIDEvent();
+  for(int ievt=0; ievt<nevt; ievt++)
+    {
+      PIDEvent* anEvent = new PIDEvent();
+      ana->run(p, anEvent);
 
-  ana.SetDetector(1.02, 20., 2., -10);
-  ana.SetCutoff(300.);
-  ana.SetdT(10.);
-  
-  ana.run(2, anEvent);
+      int nph = anEvent->GetNPhotons();
+      cout << "I have " << nph << " photons!" << endl;
+
+      cout << anEvent->Angle << endl;
+
+      PIDPhoton* photons = (PIDPhoton*)anEvent->GetPhotons();
+      for(int i=0; i<nph; i++)
+        {
+          cout << photons[i].Phi << endl;
+        }
+
+      delete[] photons;
+      delete anEvent;
+    }
 
 }
